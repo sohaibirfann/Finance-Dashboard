718d93723f1d724e3d8d3296056c8ff9f8b8f976
Sohaib Irfan <tihor.personal@gmail.com>
2023-04-02 14:50:36 +0530

    routes setup

---
+++ client/src/scenes/navbar/index.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import PixIcon from '@mui/icons-material/Pix';
+import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "@/components/FlexBetween";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  const { palette } = useTheme();
+  const [selected, steSelected] = useState("dashboard");
+  return (
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+      {/* LEFT SIDE */}
+      <FlexBetween gap="0.75rem">
+        <PixIcon />
+      </FlexBetween>
+    </FlexBetween>
+  );
+};
+
+export default Navbar;
